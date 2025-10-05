8f5cd53814e0d8e75ca133382285a4695ebb1b10
Zen <zenhanleyreisser@gmail.com>
2025-10-05 20:38:44 +0200

    functional pop ups

--- javascript/add-logs.js
+++ javascript/add-logs.js
@@ -1,8 +1,19 @@
-console.log("add-logs.js loaded in")
-let addLog = document.getElementById("add-log");
+let plus_button = document.getElementById("add-log");
+let emoji = document.getElementById("emoji");
+let language_options = document.getElementById("language-options");
+let lang = document.getElementById("lang-button");
 
-function logs() {
-    console.log("Button is functional");
+
+function revealEmoji() {
+    emoji.classList.add("reveal");
+    console.log("Reveal Emoji is Functional");
 }
 
-addLog.addEventListener("click", logs);
+function revealLang() {    
+    language_options.classList.add("reveal");
+    emoji.classList.remove("reveal");
+    console.log("Reveal Lang is Functional");
+}
+
+plus_button.addEventListener("click", revealEmoji);
+lang.addEventListener("click", revealLang);
