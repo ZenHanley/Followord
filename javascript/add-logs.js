7482ee8cf7e73c74b5241b929e7d9652ef2db23a
Zen <zenhanleyreisser@gmail.com>
2025-10-06 20:47:21 +0200

    variable feature almost working

--- javascript/add-logs.js
+++ javascript/add-logs.js
@@ -1,19 +1,44 @@
 let plus_button = document.getElementById("add-log");
 let emoji = document.getElementById("emoji");
-let language_options = document.getElementById("language-options");
-let lang = document.getElementById("lang-button");
+let study_types = document.getElementById("study-types");
+let emojiList = document.getElementsByClassName("emoji-pic");
+let studyType = document.getElementsByClassName("icon");
+let select_button = document.getElementById("select-button");
+let hoursSelected = -1;
+let typeOfStudy = -1;
 
 
+/* Reveals the emoji select pop-up */
 function revealEmoji() {
     emoji.classList.add("reveal");
     console.log("Reveal Emoji is Functional");
 }
 
-function revealLang() {    
-    language_options.classList.add("reveal");
+/* Reveals the language select pop-up and hides the emoji one */
+function revealStudyTypes() {
+    if (hoursSelected == -1) {
+        return;
+    }
     emoji.classList.remove("reveal");
+    study_types.classList.add("reveal");
     console.log("Reveal Lang is Functional");
+    console.log(hoursSelected)
 }
 
+/* adds listener events to the buttons */
 plus_button.addEventListener("click", revealEmoji);
-lang.addEventListener("click", revealLang);
+select_button.addEventListener("click", revealStudyTypes);
+
+function sendLetters(filler, filler2) {
+    for (let i=0; i < filler.length; i++) {
+        filler[i].addEventListener("click",function(){select(filler2 , i)});
+    }
+};
+
+function select(numberChange, number) {
+    numberChange = number;
+    console.log(hoursSelected)
+};
+
+sendLetters(emojiList, hoursSelected)
+sendLetters(studyType, typeOfStudy)
