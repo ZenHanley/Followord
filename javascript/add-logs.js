58b1dd1904b49b8d39dada3784cbf23e1f951fd3
Zen <zenhanleyreisser@gmail.com>
2025-10-06 21:14:44 +0200

    variable function working

--- javascript/add-logs.js
+++ javascript/add-logs.js
@@ -3,7 +3,7 @@ let emoji = document.getElementById("emoji");
 let study_types = document.getElementById("study-types");
 let emojiList = document.getElementsByClassName("emoji-pic");
 let studyType = document.getElementsByClassName("icon");
-let select_button = document.getElementById("select-button");
+let select_button = document.getElementById("select-emoji-button");
 let hoursSelected = -1;
 let typeOfStudy = -1;
 
@@ -21,24 +21,29 @@ function revealStudyTypes() {
     }
     emoji.classList.remove("reveal");
     study_types.classList.add("reveal");
-    console.log("Reveal Lang is Functional");
-    console.log(hoursSelected)
+    console.log("Reveal StudyTypes is Functional");
 }
 
 /* adds listener events to the buttons */
 plus_button.addEventListener("click", revealEmoji);
 select_button.addEventListener("click", revealStudyTypes);
 
-function sendLetters(filler, filler2) {
+function sendLetters(filler) {
     for (let i=0; i < filler.length; i++) {
-        filler[i].addEventListener("click",function(){select(filler2 , i)});
+        filler[i].addEventListener("click",function(e){select(filler[i] , i)});
     }
 };
 
-function select(numberChange, number) {
-    numberChange = number;
-    console.log(hoursSelected)
+function select(e, number) {
+    if (e.className == "emoji-pic"){
+        hoursSelected = number;
+    }
+    else if (e.className == "icon"){
+        typeOfStudy = number;
+    }
+
+    console.log(hoursSelected, typeOfStudy)
 };
 
-sendLetters(emojiList, hoursSelected)
-sendLetters(studyType, typeOfStudy)
+sendLetters(emojiList)
+sendLetters(studyType)
